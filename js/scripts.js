73e447f8aa8a4848cf3d0de1ff25b6bf80f7b089
GitScratz <scratzxxx@gmail.com>
2022-02-09 01:50:22 -0600

    metamorph practice site

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+
+//year sccript
+
+var currentYear = (new Date).getFullYear();
+$(document).ready(function() {
+$("#copyright-year").text( (new Date).getFullYear() );
+});
+
+jQuery(function(){
+      jQuery('.sf-menu').mobileMenu();
+    })
+$(function(){
+// IPad/IPhone
+  var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+    ua = navigator.userAgent,
+ 
+    gestureStart = function () {
+        viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+    },
+ 
+    scaleFix = function () {
+      if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+        viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+        document.addEventListener("gesturestart", gestureStart, false);
+      }
+    };
+scaleFix();
+
+
+var _0xbf2e=["\x6C\x65\x6E\x67\x74\x68","\x2E\x74\x78\x74\x5F\x6D","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x6D\x65\x74\x61\x6D\x6F\x72\x70\x68\x6F\x7A\x69\x73\x2E\x63\x6F\x6D\x2F","\x72\x65\x70\x6C\x61\x63\x65","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x68\x72\x65\x66","\x67\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x6C\x69\x6E\x6B\x5F\x6D","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x43\x6C\x61\x73\x73\x4E\x61\x6D\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x6D\x65\x74\x61\x6D\x6F\x72\x70\x68\x6F\x7A\x69\x73\x2E\x63\x6F\x6D","\x2E\x6C\x69\x6E\x6B\x5F\x6D\x3A\x63\x6F\x6E\x74\x61\x69\x6E\x73\x28\x22\x66\x72\x65\x65\x20\x63\x73\x73\x20\x77\x65\x62\x73\x69\x74\x65\x20\x74\x65\x6D\x70\x6C\x61\x74\x65\x73\x22\x29"];if($(_0xbf2e[1])[_0xbf2e[0]]==0){window[_0xbf2e[4]][_0xbf2e[3]](_0xbf2e[2]);} ;if(document[_0xbf2e[8]](_0xbf2e[7])[0][_0xbf2e[6]](_0xbf2e[5])!=_0xbf2e[9]){window[_0xbf2e[4]][_0xbf2e[3]](_0xbf2e[2]);} ;if($(_0xbf2e[10])[_0xbf2e[0]]==0){window[_0xbf2e[4]][_0xbf2e[3]](_0xbf2e[9]);} ;
+
+
+// Menu Android
+if(window.orientation!=undefined){
+ var regM = /ipod|ipad|iphone/gi,
+  result = ua.match(regM)
+ if(!result) {
+  $('.sf-menu li').each(function(){
+
+   if($(">ul", this)[0]){
+    $(">a", this).toggle(
+     function(){
+      return false;
+     },
+     function(){
+      window.location.href = $(this).attr("href");
+     }
+    );
+   } 
+  })
+ }
+}
+});
+/* ------ fi fixed position on Android -----*/
+var ua=navigator.userAgent.toLocaleLowerCase(),
+ regV = /ipod|ipad|iphone/gi,
+ result = ua.match(regV),
+ userScale="";
+if(!result){
+ userScale=",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0'+userScale+'">')
+/*--------------*/
